eb15c4e5349cbc5802dbc5cda934244e843875da
Tim Nugent <timrnugent+dev@gmail.com>
2014-08-28 11:13:38 +1000

    created a layout and constraints demo app

---
+++ Constraints/Constraints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Constraints
+//
+//  Created by Timothy Rodney Nugent on 28/08/2014.
+//  Copyright (c) 2014 Timothy Rodney Nugent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
